17ac33fc8b0ba35cf89e0f3f91bec8a036892bb2
yuko fuyutsuki <yuko_fuyutsuki@pmmp.tokyo>
2018-01-04 18:58:47 +0900

    [TapID] コード整形

--- TapID_v1/src/TapID/TapID.php
+++ TapID_v1/src/TapID/TapID.php
@@ -1,8 +1,6 @@
 <?php
 
-
 namespace TapID;
-
 
 use pocketmine\plugin\PluginBase;
 use pocketmine\Player;
@@ -13,42 +11,30 @@ use pocketmine\event\player\PlayerJoinEvent;
 use pocketmine\event\player\PlayerInteractEvent;
 use pocketmine\utils\Config;
 
-
-
-
-class TapID extends PluginBase implements Listener{
+class TapID extends PluginBase implements Listener {
 
 	public function onEnable(){
-
 		$this->getServer()->getPluginManager()->registerEvents($this, $this);
-if(!file_exists($this->getDataFolder()))@mkdir($this->getDataFolder()); 
-  	$this->config = new Config($this->getDataFolder() . "Item.yml", Config::YAML, array());
+		if(!file_exists($this->getDataFolder())) {
+			@mkdir($this->getDataFolder());
+		}
+		$this->config = new Config($this->getDataFolder() . "Item.yml", Config::YAML, []);
 
-if($this->config->exists("item")){
-}else{
-  
-$this->config->set("item", "347");
-$this->config->save();
-}
-
+		if(!$this->config->exists("item")){
+			$this->config->set("item", "347");
+			$this->config->save();
+		}
 	}
 
-	
-
-
-public function onBlockTap(PlayerInteractEvent $event){
-$block = $event->getBlock();
-$id = $block->getId();
-$meta = $block->getDamage();
-$player = $event->getPlayer();
-$hand = $player->getInventory()->getItemInHand()->getId();
-$con = $this->config->get("item");
-
-if($hand == $con){
-$player->sendMessage("ID $id ダメージ値 $meta");
-
-
-}
-
-}
+	public function onBlockTap(PlayerInteractEvent $event){
+		$block = $event->getBlock();
+		$id = $block->getId();
+		$meta = $block->getDamage();
+		$player = $event->getPlayer();
+		$hand = $player->getInventory()->getItemInHand()->getId();
+		$con = (int)$this->config->get("item");
+		if($hand === $con){
+			$player->sendMessage("ID $id ダメージ値 $meta");
+		}
+	}
 }
